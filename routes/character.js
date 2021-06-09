60e7edf0d83028887565525fc3258bce7c1c1dfa
matiasalvarellos <matias.alvarellos@gmail.com>
2021-06-09 17:40:04 -0300

    arreglos

--- routes/character.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const characterController = require("../controller/characterController")
-
-router.get("/create", characterController.create);
-// router.post("/create", characterController.process);
-
-module.exports = router;
--- routes/movie.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const movieController = require("../controller/movieController")
-
-router.get("/create", movieController.create);
-router.post("/create", movieController.process);
-
-module.exports = router;
